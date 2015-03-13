145ca39da81e43b5f5feaca53cf90798ddef199b
comet <baptiste.comet@cstb.fr>
2015-03-13 16:50:46 +0100

    Mise en place du Basics

---
+++ module/Blog/src/Blog/Entity/Basics.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Blog\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="basics")
+ */
+class Basics
+{
+    /** @ORM\Column(name="color", type="string", length=256) */
+    protected $color;
+
+    /** @ORM\Column(name="categories", type="text", nullable=true) */
+    protected $categories;
+
+    /** @ORM\Column(name="subtitle", type="string", nullable=false) */
+    protected $subtitle;
+
+    /**
+     * @param array $categories
+     * @return Basics $this
+     */
+    public function setCategories($categories)
+    {
+        $this->categories = json_encode($categories);
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCategories()
+    {
+        return json_decode($this->categories);
+    }
+
+    /**
+     * @param string $color
+     * @return Basics $this
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param string $subtitle
+     * @return Basics $this
+     */
+    public function setSubtitle($subtitle)
+    {
+        $this->subtitle = $subtitle;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubtitle()
+    {
+        return $this->subtitle;
+    }
+}
