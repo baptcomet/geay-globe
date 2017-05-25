807095e681d0779d5d34529da012a282c06f7740
baptistecomet <baptiste.comet@gmail.com>
2017-05-25 10:28:47 +0200

    MAJ config prod

--- config/autoload/local.production.php
+++ config/autoload/local.production.php
@@ -5,10 +5,11 @@ return array(
             'orm_default' => array(
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
                 'params' => array(
-                    'host' => '127.0.0.1',
+                    // TODO trouver des bons params de connexion pour accéder à ma BDD sur DigitalOcean
+                    'host' => '178.62.97.50',
                     'port' => '3306',
-                    'user' => 'root',
-                    'password' => '5cOsbYTTtJ',
+                    'user' => 'geaysglobe',
+                    'password' => 'flowerandguns',
                     'dbname' => 'blog',
                     'charset'  => 'utf8',
                 )
